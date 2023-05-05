42025c98418699df7f49d6e61064f63ab29a8874
mikaelangelo12 <mikaelangelo12@gmail.com>
2023-05-05 19:16:26 -0300

    Criando estrutura chamar rota api

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AppConfig } from '../../../app/app.config';
-import { catchError, throwError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
