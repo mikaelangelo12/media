3e209d711e7515afc2b40e98f52f2638a70edab4
mikaelangelo12 <mikaelangelo12@gmail.com>
2023-05-05 19:22:14 -0300

    chamando rota do backend

--- src/app/shared/services/cliente.service.ts
+++ src/app/shared/services/cliente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { HttpClient } from '@angular/common/http';
+import { AppConfig } from 'src/app/app.config';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ export class ClienteService extends ApiService<any> {
   ) {
     super(httpClient);
     super.setEndpoint('users')
+  }
+
+  listarClientes(){
+    const url = `${AppConfig.settings.urlApi}/${this.endpoint}`
+    return this.httpClient.get<any>(url)
   }
 }
